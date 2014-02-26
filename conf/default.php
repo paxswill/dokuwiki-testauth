324b385efbf599c898f86fa89163d66fe150aa57
Will Ross <paxswill@gmail.com>
2014-02-26 10:42:17 -0500

    Add plugin skeleton

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the testauth plugin
+ *
+ * @author Will Ross <paxswill@paxswill.com>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the testauth plugin
+ *
+ * @author Will Ross <paxswill@paxswill.com>
+ */
+
+
+//$meta['fixme'] = array('string');
+
